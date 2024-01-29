86cc2a653df662353eeb14c2a65d598eafaee472
johirul-islam-6 <johirulislam574206@gmail.com>
2024-01-29 23:39:50 +0600

    update

--- Components/SellerProfile/SellerProfile.jsx
+++ Components/SellerProfile/SellerProfile.jsx
@@ -3,23 +3,20 @@ import React from "react";
 export const SellerProfile = () => {
   return (
     <>
-      <div className="">
-
-<div class="flex flex-col">
-                  <span class="text-gray-700 uppercase font-bold tracking-wider mb-2">
-                    information
-                  </span>
-                  <ul>
-                    <li class="mb-2">joining date : 02/02/2024</li>
-                    <li class="mb-2">Roll : 574206</li>
-                    <li class="mb-2">Department : Computer</li>
-                    <li class="mb-2">Gender : Male</li>
-                    <li class="mb-2">Phone : 01824842336</li>
-                    <li class="mb-2">Email : johir@gmail.com</li>
-                  </ul>
-                </div>
-              </div>
-
+      <div className="px-5">
+        <div class="flex flex-col">
+          <span class="text-[#fff] uppercase font-bold tracking-wider mb-2">
+            information
+          </span>
+          <ul>
+            <li class="mb-2 text-[#fff] ">joining date : 02/02/2024</li>
+            <li class="mb-2 text-[#fff] ">Roll : 574206</li>
+            <li class="mb-2 text-[#fff] ">Department : Computer</li>
+            <li class="mb-2 text-[#fff] ">Gender : Male</li>
+            <li class="mb-2 text-[#fff] ">Phone : 01824842336</li>
+            <li class="mb-2 text-[#fff] ">Email : johir@gmail.com</li>
+          </ul>
+        </div>
       </div>
     </>
   );
