d8e326af9c60ccba0e9efa933c341d5a202e7b9a
johirul-islam-6 <johirulislam574206@gmail.com>
2024-02-02 16:24:26 +0600

    update

--- Components/Home/Contact/Contact.jsx
+++ Components/Home/Contact/Contact.jsx
@@ -16,7 +16,7 @@ export const Contact = () => {
         {/* ------- message ---- */}
         <div class="max-w-screen-xl  mx-auto p-5">
           <div class="flex flex-col-reverse md:grid grid-cols-1 md:grid-cols-12 border">
-            <div class="bg-[#563A9F] md:col-span-4 p-1 text-white">
+            <div class="bg-[#563A9F] md:h-[75vh] rounded-lg md:col-span-4 p-1 text-white">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9861.890915948505!2d90.39878777633939!3d24.74092141469946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x37564f0b64eb63f3%3A0xb114002e267aea46!2z4KaV4Kau4KeN4Kaq4Ka_4KaJ4Kaf4Ka-4KawIOCmoeCmv-CmquCmvuCmsOCnjeCmn-CmruCnh-CmqOCnjeCmnw!5e0!3m2!1sbn!2sbd!4v1706865109672!5m2!1sbn!2sbd"
                 className="w-[100%] h-[100%]"
@@ -26,8 +26,8 @@ export const Contact = () => {
               ></iframe>
             </div>
             <form
-              action="https://fabform.io/f/{form-id}"
-              method="post"
+              action="https://getform.io/f/346003ba-86ef-4b17-9fc1-6ebd93a3af1a"
+              method="POST"
               class="md:col-span-8 p-10"
             >
               <div class="flex flex-wrap -mx-3 ">
@@ -40,9 +40,10 @@ export const Contact = () => {
                   </label>
                   <input
                     class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                    id="grid-last-name"
+                    id="name"
+                    name="name"
                     type="text"
-                    placeholder="Doe"
+                    placeholder="Md.Johirul Islam (rasel)"
                   />
                 </div>
                 <div class="w-full md:w-1/2 px-3">
@@ -56,7 +57,7 @@ export const Contact = () => {
                     class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                     id="grid-last-name"
                     type="text"
-                    placeholder="Doe"
+                    placeholder="Maskanda, Mymensingh"
                   />
                 </div>
               </div>
@@ -70,7 +71,8 @@ export const Contact = () => {
                   </label>
                   <input
                     class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                    id="grid-email"
+                    id="email"
+                    name="email"
                     type="email"
                     placeholder="ex: rasel@gamil.com"
                   />
@@ -86,6 +88,8 @@ export const Contact = () => {
                     Your Message
                   </label>
                   <textarea
+                    id="message"
+                    name="message"
                     rows="10"
                     class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                   ></textarea>
