a709de1193fa9a530f56cb545dd3dadb31bd4449
johirul-islam-6 <johirulislam574206@gmail.com>
2024-02-10 14:21:29 +0600

    update

--- Components/Home/Products/Diploma/DiplomaBookSlider.jsx
+++ Components/Home/Products/Diploma/DiplomaBookSlider.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Slider from "react-slick";
-import { ImageCard } from "./ImageCard";
 import "./Image.css";
 import Link from "next/link";
 import Image from "next/image";
@@ -10,7 +9,7 @@ const DiplomaBookSlider = ({ ResultBooks, Loding }) => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 5,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
@@ -86,23 +85,45 @@ const DiplomaBookSlider = ({ ResultBooks, Loding }) => {
         {Array.isArray(ResultBooks) &&
           ResultBooks.map((item, index) => (
             <>
-              <div key={item?._id} class="card">
-                <Image
-                  width={250}
-                  height={20}
-                  className="card-image absolute top-0 p-5"
-                  src={item?.bookImage} // Use item.bookImage instead of ResultBooks?.bookImage
-                  alt=""
-                />
-                <div class="content ">
-                  <h2 class="title p-2 pt-0 ">{item?.bookName}</h2>
-                  <p class="copy py-2 ">
-                    Check out all of these gorgeous mountain trips with
-                    beautiful views of, you guessed it, the mountains
-                  </p>
-                  <Link href={"/detailsBook/1"}>
-                    <button class="btn">View Details</button>
-                  </Link>
+              <div className="card ">
+                <div key={item?._id} class="shadow-card-slider pe-20">
+                  <div className="w-[100%] flex justify-start items-center mx-auto ">
+                    <Image
+                      width={0}
+                      height={0}
+                      className="w-[100%] h-[100%] absolute top-5 pe-10 "
+                      src={item?.bookImage} // Use item.bookImage instead of ResultBooks?.bookImage
+                      alt=""
+                    />
+                  </div>
+                  <div class="contents ">
+                    <h2 class="title p-2 py-3 w-[100%] ">
+                      {item?.bookName?.slice(0, 20)}
+                    </h2>
+                    <div className="all-button-courses flex flex-wrap justify-center items-center mt-5 gap-2 ">
+                      <p className="bg-[#0000] flex gap-[2px] items-center px-2 py-[4px] border-2 border-[#fff] rounded-md text-[10px] lg:text-[12px] font-[700] text-[#fff]">
+                        {item?.publication}
+                      </p>
+                      <p className="bg-[#0000] flex gap-[2px] items-center px-2 py-[4px] border-2 border-[#fff] rounded-md text-[10px] lg:text-[12px] font-[700] text-[#fff]">
+                        {item?.createdAt?.slice(0, 10)}
+                      </p>
+                      <p className="bg-[#0000] flex gap-[2px] items-center px-2 py-[4px] border-2 border-[#fff] rounded-md text-[10px] lg:text-[12px] font-[700] text-[#fff] ">
+                        {item?.subjectCode?.slice(0, 5)}
+                      </p>
+
+                      <p className="bg-[#0000] flex gap-[2px] items-center px-2 py-[4px] border-2 border-[#fff] rounded-md text-[10px] lg:text-[12px] font-[700] text-[#fff]">
+                        {item?.semester}
+                      </p>
+                      <p className="bg-[#0000] flex gap-[2px] items-center px-2 py-[4px] border-2 border-[#fff] rounded-md text-[10px] lg:text-[12px] font-[700] text-[#fff]">
+                        {item?.location}
+                      </p>
+                    </div>
+                    <Link href={"/detailsBook/1"}>
+                      <button class="px-8 bg-white text-black py-2 mt-5 font-[600]">
+                        View Details
+                      </button>
+                    </Link>
+                  </div>
                 </div>
               </div>
             </>
